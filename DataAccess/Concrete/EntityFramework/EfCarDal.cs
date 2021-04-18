6c85c6dbbb8ed2cdce60d4105a420903383ed00d
Hasim berke özden <73190494+Berke35-hash@users.noreply.github.com>
2021-04-18 14:25:13 +0300

    Update EfCarDal.cs

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -35,17 +35,7 @@ namespace DataAccess.Concrete.EntityFramework
                 return result.Where(filter).ToList();
             }
         }
-        public List<CarDetailDto> GetCarDetails()
-        {
-            using (ReCapDemoContext context = new ReCapDemoContext())
-            {
-                var result = from p in context.Cars
-                             join k in context.Colors on p.ColorID equals k.ID
-                             join l in context.Brands on p.BrandID equals l.ID
-                             select new CarDetailDto { ID = p.ID, ColorName = k.ColorName, BrandName = l.BrandName, DailyPrice = p.DailyPrice, Description = p.Description, ModelYear = p.ModelYear };
-                return result.ToList();
-            }
-        }
+
         public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
         {
             using (ReCapDemoContext context = new ReCapDemoContext())
