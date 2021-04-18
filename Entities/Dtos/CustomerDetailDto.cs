b121f044f8ffee635052d0c507de620b5a9d1eb6
Hasim berke özden <73190494+Berke35-hash@users.noreply.github.com>
2021-04-18 14:23:01 +0300

    Update CustomerDetailDto.cs

--- Entities/Dtos/CustomerDetailDto.cs
+++ Entities/Dtos/CustomerDetailDto.cs
@@ -10,7 +10,6 @@ namespace Entities.Dtos
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string NickName { get; set; }
         public string Email { get; set; }
         public string CompanyName { get; set; }
     }
